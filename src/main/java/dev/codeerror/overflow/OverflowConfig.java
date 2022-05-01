a5af4fc00fc59158b8a899f507dfc2e1516b07cf
CodeTheDev <code@codeslab.ca>
2022-05-01 12:19:57 -0400

    Create OverflowConfig class
    - Handles saving and loading server configuration file.
    - Provides getters for access to configuration data within other classes.

---
+++ src/main/java/dev/codeerror/overflow/OverflowConfig.java
@@ -0,0 +1,96 @@
+package dev.codeerror.overflow;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Properties;
+
+public class OverflowConfig {
+
+    private final Properties config = new Properties();
+    private final Logger logger = LoggerFactory.getLogger(OverflowConfig.class);
+
+    private String serverBrand;
+    private String serverAddress;
+    private int serverPort;
+    private int compressionThreshold;
+    private int viewDistance;
+    private int playerLimit;
+    private String motd;
+    private String tabHeader;
+    private String tabFooter;
+    private boolean velocityEnabled;
+    private String velocitySecret;
+
+    public void saveDefaultConfig() {
+        File defaultConfig = new File(new File(OverflowConfig.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParentFile(), "server.properties");
+        if (!defaultConfig.exists()) {
+            try {
+                Files.copy(Objects.requireNonNull(OverflowConfig.class.getResourceAsStream("/server.properties")), Paths.get(defaultConfig.getPath()));
+                logger.info("Created default configuration.");
+            } catch (Exception e) {
+                logger.error("Unable to create default configuration.", e);
+            }
+        }
+    }
+
+    public void loadConfig() {
+        try (FileReader in = new FileReader("server.properties")) {
+            config.load(in);
+            serverBrand = config.getProperty("server-brand", "OverflowLimbo");
+            serverAddress = config.getProperty("server-address", "127.0.0.1");
+            serverPort = Integer.parseInt(config.getProperty("server-port", "25565"));
+            compressionThreshold = Integer.parseInt(config.getProperty("compression-threshold", "-1"));
+            viewDistance = Integer.parseInt(config.getProperty("view-distance", "2"));
+            playerLimit = Integer.parseInt(config.getProperty("max-players", "-1"));
+            motd = config.getProperty("motd", "<dark_aqua><italic>An OverflowLimbo Server");
+            tabHeader = config.getProperty("tablist-header", "");
+            tabFooter = config.getProperty("tablist-footer", "");
+            velocityEnabled = Boolean.parseBoolean(config.getProperty("velocity", "false"));
+            velocitySecret = config.getProperty("velocity-secret", "NONE");
+            logger.info("Loaded configuration.");
+        } catch (IOException e) {
+            logger.error("Unable to load/parse configuration.", e);
+        }
+    }
+
+    public String getServerBrand() {
+        return serverBrand;
+    }
+    public String getServerAddress() {
+        return serverAddress;
+    }
+    public int getServerPort() {
+        return serverPort;
+    }
+    public int getCompressionThreshold() {
+        return compressionThreshold;
+    }
+    public int getViewDistance() {
+        return viewDistance;
+    }
+    public int getPlayerLimit() {
+        return playerLimit;
+    }
+    public String getMotd() {
+        return motd;
+    }
+    public String getTabHeader() {
+        return tabHeader;
+    }
+    public String getTabFooter() {
+        return tabFooter;
+    }
+    public boolean isVelocityEnabled() {
+        return velocityEnabled;
+    }
+    public String getVelocitySecret() {
+        return velocitySecret;
+    }
+}
