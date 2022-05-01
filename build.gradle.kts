e240b3b14f08aba928cbe2bb588b0480d97c79ce
CodeTheDev <code@codeslab.ca>
2022-05-01 12:15:12 -0400

    Updated build script
    - Added shadow plugin.
    - Added Minestom dependency.
    - Added MiniMessage dependency.
    - Added shadowJar task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,28 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
 }
 
 group = "dev.codeerror"
-version = "1.0-SNAPSHOT"
+version = "1.0"
+
+apply(plugin = "com.github.johnrengelman.shadow")
 
 repositories {
     mavenCentral()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.github.Minestom:Minestom:master-SNAPSHOT")
+    implementation("net.kyori:adventure-text-minimessage:4.10.1")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+    manifest {
+        attributes("Main-Class" to "dev.codeerror.overflow.OverflowLimbo")
+    }
 }
