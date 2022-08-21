4e683aec567ae832bb0e4e7cbb31f95f6bd57d04
CodeTheDev <code@codeslab.ca>
2022-08-21 02:31:57 -0400

    Removed repeating chat message feature
    - Removed repeating chat message feature.
    - Fixed velocity forwarding support check.

--- src/main/java/dev/codeerror/overflow/OverflowLimbo.java
+++ src/main/java/dev/codeerror/overflow/OverflowLimbo.java
@@ -2,7 +2,6 @@ package dev.codeerror.overflow;
 
 import dev.codeerror.overflow.util.OverflowUtils;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
@@ -16,8 +15,6 @@ import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.ping.ResponseData;
-import net.minestom.server.timer.Scheduler;
-import net.minestom.server.timer.TaskSchedule;
 import net.minestom.server.utils.NamespaceID;
 import net.minestom.server.world.DimensionType;
 import net.minestom.server.world.DimensionTypeManager;
@@ -73,7 +70,6 @@ public class OverflowLimbo {
         logger.info("Initializing world...");
         InstanceContainer world = MinecraftServer.getInstanceManager().createInstanceContainer(THE_VOID);
         GlobalEventHandler events = MinecraftServer.getGlobalEventHandler();
-        Scheduler scheduler = MinecraftServer.getSchedulerManager();
         Pos spawnPos = new Pos(0.5, 128, 0.5);
 
         world.setTime(0);
@@ -86,7 +82,6 @@ public class OverflowLimbo {
         logger.info("Registering event listeners...");
         events.addListener(ServerListPingEvent.class, event -> {
             ResponseData response = new ResponseData();
-
             response.setVersion(serverVersion);
             response.setMaxPlayer(config.getPlayerLimit() == -1 ? event.getResponseData().getOnline() + 1 : config.getPlayerLimit());
             response.setDescription(serverMotd);
@@ -99,7 +94,6 @@ public class OverflowLimbo {
         });
         events.addListener(PlayerLoginEvent.class, event -> {
             Player player = event.getPlayer();
-
             player.setAutoViewable(false);
             player.setRespawnPoint(spawnPos);
             event.setSpawningInstance(world);
@@ -109,18 +103,10 @@ public class OverflowLimbo {
         }
         events.addListener(PlayerSpawnEvent.class, event -> {
             Player player = event.getPlayer();
-
             player.setGameMode(GameMode.SPECTATOR);
             player.setNoGravity(true);
             player.setInvulnerable(true);
             player.setInvisible(true);
-
-            if (config.isVelocityEnabled()) {
-                player.sendMessage(Component
-                        .text("Servers are currently full! You have been temporarily placed into an overflow limbo server.")
-                        .color(NamedTextColor.YELLOW)
-                );
-            }
         });
         events.addListener(PlayerMoveEvent.class, event -> event.setCancelled(true));
         events.addListener(PlayerChatEvent.class, event -> event.setCancelled(true));
@@ -129,22 +115,10 @@ public class OverflowLimbo {
             logger.info(player.getUsername() + " [" + player.getPlayerConnection().getRemoteAddress() + "] disconnected.");
         });
 
-        // Scheduler Tasks
-        logger.info("Registering scheduler tasks...");
-        if (config.isVelocityEnabled()) {
-            scheduler.submitTask(() -> {
-                world.sendMessage(Component
-                        .text("Servers are currently full! You have been temporarily placed into an overflow limbo server.")
-                        .color(NamedTextColor.YELLOW)
-                );
-                return TaskSchedule.minutes(1);
-            });
-        }
-
         // Velocity Forwarding Support
         boolean velocityEnabled = config.isVelocityEnabled();
         String velocitySecret = config.getVelocitySecret();
-        if (velocityEnabled && !velocitySecret.equalsIgnoreCase("NONE") && !velocitySecret.isBlank()) {
+        if (velocityEnabled && !velocitySecret.isBlank()) {
             VelocityProxy.enable(velocitySecret);
             logger.info("Velocity modern forwarding support enabled.");
         }
